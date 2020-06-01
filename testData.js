f046803a68e5056e925ec168c90cf5c46a042955
Ailsa Oneil-Dunne <oneil-da2@bsdvt.org>
2020-06-01 18:43:02 +0000

    Load persons

---
+++ testData.js
@@ -0,0 +1,5 @@
+Help Requests: [["1-0008675309","Toilet Paper","5","1","3/2/20",null,null],["2-8357502412","Steaks","3","2","3/2/20",null,null]]
+
+Persons: [["1-0008675309","Bob","Walter","bob@yahoo.com","0008675309","Burlington"],["2-8357502412","Napoleon","Dynamite","therose@yahoo.com","8357502412","Williston"]]
+
+Needs: ["Shopping","Grocery delivery","Medical supplies","Meal delivery","Meal making","Child care/aid","Wellness checks","Mental health support","Financial support"]
